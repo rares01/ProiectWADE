a418fdfbddc3f410b0182d90d36f0e69e3850c7e
adrian-vatui <adrianvatui9989@yahoo.com>
2024-02-04 20:44:19 +0200

    Bug fixes

--- wed-BE/src/main/java/com/wed/authentication/DisabledRequestsFilter.java
+++ wed-BE/src/main/java/com/wed/authentication/DisabledRequestsFilter.java
@@ -24,7 +24,8 @@ public class DisabledRequestsFilter extends OncePerRequestFilter {
             "/api/user",
             "/api/user/preferences",
             "/api/user/firstLogin",
-            "/api/user/getPreferences"
+            "/api/user/getPreferences",
+            "/api/sparql"
     );
 
     @Override
